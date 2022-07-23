4381efb2d2bfcb742f9dd22a7192764ab8786215
Polina Che <perhaybe@gmail.com>
2022-07-23 22:57:21 +0300

    fixed: bugs after the 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 let editButton = document.querySelector(".user__edit-button");
-let popUpWindowOpened = document.querySelector(".popup_opened");
+let popUpWindow = document.querySelector(".popup");
 let closePopUpButton = document.querySelector(".popup__close-button");
 
 let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__name-input");
-let jobInput = document.querySelector(".popup__job-input");
+let nameInput = document.querySelector(".popup_name-input");
+let jobInput = document.querySelector(".popup_job-input");
 
 let nameValue = nameInput.value;
 let jobValue = jobInput.value;
@@ -13,13 +13,16 @@ let userName = document.querySelector(".user__name");
 let userJob = document.querySelector(".user__job");
 
 function showPopUp() {
-  popUpWindowOpened.style.display = "flex";
+  popUpWindow.classList.add("popup_opened");
+  popUpWindow.style.display = "flex";
+
   nameInput.value = userName.textContent;
   jobInput.value = userJob.textContent;
 }
 
 function closePopUp() {
-  popUpWindowOpened.style.display = "none";
+  popUpWindow.classList.remove("popup_opened");
+  popUpWindow.style.display = "none";
 }
 
 function formSubmitHandler(evt) {
@@ -28,11 +31,9 @@ function formSubmitHandler(evt) {
   nameValue = nameInput.value;
   jobValue = jobInput.value;
 
-  userName = document.querySelector(".user__name");
-  userJob = document.querySelector(".user__job");
-
   userName.textContent = nameValue;
   userJob.textContent = jobValue;
+
   closePopUp();
 }
 
